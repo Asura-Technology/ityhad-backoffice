549f6dcadd479f392d6d0ee046137e9bf5236f53
zakaria.elhedadi <zakaria.elhedadi@gmail.com>
2025-05-16 23:03:59 +0100

    update

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -2,19 +2,12 @@
 
 import dataProviderHasura, { GraphQLClient } from "@refinedev/hasura";
 
-if (!process.env.NEXT_PUBLIC_HASURA_API_URL) {
-  throw new Error("NEXT_PUBLIC_HASURA_API_URL is not defined");
-}
-
-if (!process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET) {
-  throw new Error("NEXT_PUBLIC_HASURA_ADMIN_SECRET is not defined");
-}
-
 export const client = new GraphQLClient(
-  process.env.NEXT_PUBLIC_HASURA_API_URL,
+  process.env.NEXT_PUBLIC_HASURA_API_URL as string,
   {
     headers: {
-      "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET,
+      "x-hasura-admin-secret": process.env
+        .NEXT_PUBLIC_HASURA_ADMIN_SECRET as string,
     },
   }
 );
