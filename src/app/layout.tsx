364c7dedf3a9f528548ddbace0ddf78b931f72e2
zakaria.elhedadi <zakaria.elhedadi@gmail.com>
2025-05-18 23:49:52 +0100

    fix'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Script src="/runtime-env.js" strategy="beforeInteractive" />
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <Script src="/runtime-env.js" strategy="beforeInteractive" />
+      </head>
       <body>
         <Suspense>
           {/* <GitHubBanner /> */}
